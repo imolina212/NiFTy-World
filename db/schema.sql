28c681b373adf9231f73375c2a3431b19f1c1a50
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-12 16:42:49 -0400

    fixed db issues

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS nifty_world_dev;
 CREATE DATABASE nifty_world_dev;
-/c nifty_world_dev;
+\c nifty_world_dev;
 CREATE TABLE content (
     id SERIAL PRIMARY KEY,
     lesson_categories TEXT,
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-/c nifty_world_dev;
+\c nifty_world_dev;
 
 INSERT INTO content (lesson_categories, videos, reading_material) VALUES
 ('NFT', 'opensea.com', 'An NFT is a non-fungible token'),
