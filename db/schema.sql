5b19baf2f851b0c0add85580722133df213d22b1
caveman49 <caveman4949@gmail.com>
2022-05-12 15:44:31 -0400

    updated db and queries

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS nifty_world_dev;
+CREATE DATABASE nifty_world_dev;
+/c nifty_world_dev;
+CREATE TABLE content (
+    id SERIAL PRIMARY KEY,
+    lesson_categories TEXT,
+    videos TEXT,
+    reading_material TEXT
+)
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+/c nifty_world_dev;
+
+INSERT INTO content (lesson_categories, videos, reading_material) VALUES
+('NFT', 'opensea.com', 'An NFT is a non-fungible token'),
+('Cryto', 'https://www.coinbase.com/', 'A cryptocurrency, crypto-currency, crypto, or coin is a digital currency designed to work as a medium of exchange through a computer network that is not reliant on any central authority, such as a government or bank, to uphold or maintain it.')
