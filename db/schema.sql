80cf9aa328c2f5636295be7703c17ef063232f1e
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-16 16:57:24 -0400

    added quizzes table

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,18 @@
 DROP DATABASE IF EXISTS nifty_world_dev;
 CREATE DATABASE nifty_world_dev;
 \c nifty_world_dev;
+
 CREATE TABLE content (
     id SERIAL PRIMARY KEY,
     lesson_categories TEXT,
     videos TEXT,
     reading_material TEXT
-)
+);
+
+CREATE TABLE quizzes (
+    id SERIAL PRIMARY KEY,
+    category TEXT,
+    question TEXT,
+    answer TEXT,
+    content_id INTEGER REFERENCES content (id)
+);
