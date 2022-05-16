d4b6a5aeff1bb22843651631ee7817292f8ce04d
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-16 16:57:56 -0400

    added quizzes seed

--- db/seed.sql
+++ db/seed.sql
@@ -2,3 +2,6 @@
 
 INSERT INTO content (lesson_categories, videos, reading_material) VALUES
 ('NFT', 'opensea.com', 'An NFT is a non-fungible token');
+
+INSERT INTO quizzes (category , question, answer) VALUES
+('NFT', 'What is a NFT?', 'An NFT is a non-fungible token')
