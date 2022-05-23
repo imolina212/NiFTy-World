5dcb02b7a1a012381c769034f1f2cef44c2cc06e
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-05-23 12:58:44 -0400

    Add back-end content for what is cyrpto for all tables

--- db/seed.sql
+++ db/seed.sql
@@ -7,9 +7,12 @@ INSERT INTO categories (name) VALUES
 
 INSERT INTO lessons (lesson_title, videos, reading_material) VALUES
 ('NFT', 'opensea.com', 'An NFT is a non-fungible token');
+('Cryptocurrency', 'coinbase.com', 'Unlike NFTs, Physical money and cryptocurrencies are “fungible,” meaning they can be traded or exchanged for one another. They are also equal in value—one dollar is always worth another dollar; one Bitcoin is always equal to another Bitcoin. ');
 
 INSERT INTO questions ( question_text, answer) VALUES
-('What is a NFT?', 'An NFT is a non-fungible token');
+('What is a NFT?', 'An NFT is a non-fungible token'),
+('What is Cryptocurrency?', '
+A cryptocurrency is an encrypted data string that denotes a unit of currency. It is monitored and organized by a peer-to-peer network called a blockchain, which also serves as a secure ledger of transactions, e.g., buying, selling, and transferring.');
 
 INSERT INTO comments (name, post, date) VALUES
 ('Shawnee', 'I need some cryto, gimme some now!!', '2022-05-17'),
