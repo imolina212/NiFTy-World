ae5b84f78cbe548925c14df02036419efb33a2b8
Taylor W <taylorwilliams@pursuit.org>
2022-06-04 12:09:52 -0400

    Fixed backend

--- db/seed.sql
+++ db/seed.sql
@@ -9,7 +9,6 @@ INSERT INTO lessons (lesson_title, videos_url, reading_material, categories_id) 
 ('What is an NFT?', '/videos/nftvideo.mp4', 'An NFT is a non-fungible token', 1),
 ('What is fungibility?', 'opensea.com', 'Fungibility is the ability of a good or asset to be interchanged with other individual goods or assets of the same type.', 1),
 ('What are some examples of NFT?', 'opensea.com', 'Tweets, videos, digital art and even source code.', 1),
-
 ('Where can I show off my NFTs?', 'opensea.com', 'There are several ways to show off your newly purchased NFT, including digital enviroments such as the metaverse', 1),
 ('What is cryptocurrency?', 'Cryprocurrency.com', 'A digital currency in which transactions are verified and records maintained by a decentralized system using cryptography.', 2),
 ('Where can I store my cryptocurrency?', 'https://cryptonews.com/guides/how-to-store-cryptocurrency-safely.htm', 'In order to store your cryptocurrency safely you will need to get a crypto wallet. There are four types of cryptocurrency wallets: paper, hardware, cloud, and online. Each have there own pros and cons like wallets always being connected to the internet (hot wallet) for quick and easy access but might not be the most secure option. On the other hand, if you went with an offline (cold wallet), its pros will be that its one of the more secure options while cons being its not the quickest option available so it might negatively affect those who buy and sell crypto very frequently so choose wisely!', 2),
@@ -21,14 +20,17 @@ INSERT INTO lessons (lesson_title, videos_url, reading_material, categories_id) 
 
 INSERT INTO questions ( question_text, answer, lessons_id) VALUES
 ('What is a NFT?', 'An NFT is a non-fungible token', 1),
-('What is a NFT Marketplace?', 'An NFT Marketplace is a platform where you can buy, sell or auction NFTs', 1),
-('What is a Crypto?', 'An NFT is a non-fungible token', 2),
-('What are the different types of crypto?', 'Bitcon, Doge and Sheba', 2),
+('What is fungibility?', 'Fungibility is the ability of a good or asset to be interchanged with other individual goods or assets of the same type.', 1),
+('What are some examples of NFT?', 'Tweets, videos, digital art and even source code.', 1),
+('Where can I show off my NFTS?', 'There are several ways to show off your newly purchased NFT, including digital enviroments such as the metaverse', 1),
+('What is a Crypto?', 'A digital currency in which transactions are verified and records maintained by a decentralized system using cryptography.', 2),
+('How many type of cryptocurrency wallets are there?', 'four', 2),
 ('Who created Bitcoin?', 'Bitcoin was created by Satoshi Nakamoto', 2),
 -- ('Where does crypto live?', 'Crypto lives on the blockchain', 2),
 -- ('What is savings?', 'Savings is the ability to compound money', 3),
-('What is a trust?', 'A trust is when you trust people', 3),
-('What is a Finance?', 'An NFT is a non-fungible token', 3),
+('What is a Finance?', 'Finance is the management of large amounts of money.', 3),
+('What is an asset', 'An asset is any resource or good used to create cash flow.', 3),
+('What is net worth?', 'Your net worth is the amount of assets you currently have minus the amount of liabilities you currently own.', 3),
 ('What is a debit purchase?', 'When you purchase something when you actually have the money for it.', 3);
 
 INSERT INTO comments (name, post, date) VALUES
